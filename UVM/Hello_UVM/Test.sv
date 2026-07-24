1127c2cc0324182ac7cb4a24bb1c7fb5d45dab88
SaiLakshmi-S <146068232+SaiLakshmi-S@users.noreply.github.com>
2026-07-24 12:08:49 +0530

    Update Test.sv

--- UVM/Hello_UVM/Test.sv
+++ UVM/Hello_UVM/Test.sv
@@ -1,1 +1,15 @@
-
+class My_test extends uvm_test;
+  
+  `uvm_component_utils(My_test) // registering the class with UVM factory
+  
+  My_Env envr;
+  function new(string name , uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    envr = My_Env :: type_id :: create("envr", this );
+  endfunction
+  
+endclass
