193ee07863bd5ce80b5770629034cabc8875fc0e
SaiLakshmi-S <146068232+SaiLakshmi-S@users.noreply.github.com>
2026-07-24 12:08:14 +0530

    Update Top.sv

--- UVM/Hello_UVM/Top.sv
+++ UVM/Hello_UVM/Top.sv
@@ -1,1 +1,22 @@
 
+package my_pck;
+
+	
+ 	import uvm_pkg::*;
+	`include "uvm_macros.svh"
+	`include "Env.sv";
+	`include "Test.sv";
+
+endpackage
+
+module top;
+   
+  import uvm_pkg::*;
+  import my_pck::*;
+  `include "uvm_macros.svh";
+  
+  initial
+    begin
+      run_test("My_test");
+    end
+endmodule
